e62931a9a3f6cfbefd90ce6fef188612c5b9d553
Manohar Prabhu <manoharagent13@gmail.com>
2025-12-27 00:22:49 +0530

    add cors config

--- src/main/java/com/ag13/mafia/rest/mafiaclientrest/controllers/LobbyController.java
+++ src/main/java/com/ag13/mafia/rest/mafiaclientrest/controllers/LobbyController.java
@@ -5,9 +5,7 @@ import com.ag13.mafia.rest.mafiaclientrest.DTO.lobby.LobbyCreateRequest;
 import com.ag13.mafia.rest.mafiaclientrest.DTO.lobby.LobbyCreateResponse;
 import com.ag13.mafia.rest.mafiaclientrest.service.LobbyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LobbyController {
