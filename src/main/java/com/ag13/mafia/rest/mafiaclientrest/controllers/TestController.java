bcf558ad494ebc8d74ec9422294ac737ded1a505
Manohar Prabhu <manoharagent13@gmail.com>
2026-01-01 18:23:30 +0530

    add test names to test controller

--- src/main/java/com/ag13/mafia/rest/mafiaclientrest/controllers/TestController.java
+++ src/main/java/com/ag13/mafia/rest/mafiaclientrest/controllers/TestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class TestController {
     @Autowired
@@ -13,14 +17,31 @@ public class TestController {
     @Autowired
     GameController gameController;
 
+    private String[] playerNames = new  String[]{
+            "Tejram",
+            "Harish",
+            "Naman",
+            "Parul",
+            "Nikhil",
+            "Saurabh",
+            "Sandeep",
+            "Rohit",
+            "Priya",
+            "Keshav",
+            "Shravan",
+            "Subal"
+    };
+
     @GetMapping(path = "/adddummyplayers")
     public String joinLobby(@RequestParam String lobbyId, @RequestParam int size) {
         var successCount = 0;
         var failureCount = 0;
+        List<String> list = Arrays.asList(playerNames);
+        Collections.shuffle(list);
         for(var i = 0; i < size; i++) {
             var playerJoinRequest = new LobbyJoinRequest();
             playerJoinRequest.setLobbyId(lobbyId);
-            playerJoinRequest.setPlayerName(String.valueOf(i));
+            playerJoinRequest.setPlayerName(list.get(i % list.size()));
             var response = lobbyController.joinLobby(playerJoinRequest);
             if(response.isSuccess()) {
                 successCount++;
